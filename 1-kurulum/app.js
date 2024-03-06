636bd63a631dea797c14a251e5a2c3f3e1577bfc
busra <b.busraayilmaz@gmail.com>
2024-03-06 23:34:40 +0300

    create-typescript

---
+++ 1-kurulum/app.js
@@ -0,0 +1,13 @@
+console.log("hello world");
+console.log("hello world 12");
+console.log("hello world 123");
+console.log("hello  123");
+console.log("hello  123s");
+//data annotation
+var age = 29;
+var firstname = "Büşra";
+var latstname = "Yilmaz";
+var isUpdated = true;
+function display(id, name) {
+    console.log("Id =" + id + "name =" + name);
+}
---
+++ 1-kurulum/app.ts
@@ -0,0 +1,16 @@
+console.log("hello world");
+console.log("hello world 12");
+console.log("hello world 123");
+console.log("hello  123");
+console.log("hello  123s");
+
+//data annotation
+
+let age: number = 29;
+let firstname: string = "Büşra";
+let latstname: string = "Yilmaz";
+let isUpdated: boolean = true;
+
+function display(id: any, name: string) {
+  console.log("Id =" + id + "name =" + name);
+}
